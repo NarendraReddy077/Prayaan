2ab6adec2d7ac80524538b35154a98fc1ff8a158
NarendraReddy077 <narendrareddymolakala@gmail.com>
2025-11-14 21:51:27 +0530

    upgraded UI

--- Prayaan/settings.py
+++ Prayaan/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Easy.urls'
+ROOT_URLCONF = 'Prayaan.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Easy.wsgi.application'
+WSGI_APPLICATION = 'Prayaan.wsgi.application'
 
 
 # Database
